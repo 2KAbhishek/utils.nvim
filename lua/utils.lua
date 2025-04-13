c02c5119abb79bdf37c2041995893079647a0211
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-04-13 20:13:07 +0530

    feat: add is available check for modules

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,6 +1,17 @@
-return {
-    setup = function(opts)
-        require('utils.config').setup(opts)
-        require('utils.commands').setup()
-    end,
-}
+local M = {}
+
+M.setup = function(opts)
+    require('utils.config').setup(opts)
+    require('utils.commands').setup()
+end
+
+M.is_available = function(module)
+    if not module then
+        return false
+    end
+
+    local ok = pcall(require, module)
+    return ok
+end
+
+return M
